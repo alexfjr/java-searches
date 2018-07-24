b79a9ac01e8f23896ceab419acd27be603b9cabc
Alexandro Ferreira Junior <alexandro_fjr@hotmail.com>
2018-07-24 14:20:50 -0300

    Scripts para compilar e executar exemplo de uso do JNativeHook adicionados

---
+++ jni-searches/jnativehook-2.1.0/comp.bat
@@ -0,0 +1,1 @@
+javac -cp .;D:\Programs\libs\jnativehook-2.1.0.jar GlobalKeyListenerExample.java
---
+++ jni-searches/jnativehook-2.1.0/run.bat
@@ -0,0 +1,1 @@
+java -cp .;D:\Programs\libs\jnativehook-2.1.0.jar GlobalKeyListenerExample 2> err.log
